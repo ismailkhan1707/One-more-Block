10ad2b040373abe682a1e9d6bf8cd34e07bbe4fb
ismailkhan1707 <ismailkb2004@gmail.com>
2026-07-22 03:08:49 +0500

    Spawn Timer added

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -11,10 +11,8 @@ public partial class GameManager : Node
 	[Export]
 	private Node2D _world;
 
-	[Export]
-	private Timer _spawnTimer;
-	
-	private Block _currentBlock;	
+	private Block _currentBlock;
+	private bool _waitingForLanding = false;
 
 	public override void _Ready()
 	{
@@ -29,10 +27,7 @@ public partial class GameManager : Node
 
 		_currentBlock.GlobalPosition = _crane.BlockSpawn.GlobalPosition;
 	}
-	private void _on_spawn_timer_timeout()
-	{
-		SpawnBlock();
-	}
+
 	public override void _Process(double delta)
 	{
 		if (_currentBlock != null && _currentBlock.Freeze)
@@ -42,8 +37,22 @@ public partial class GameManager : Node
 			if (Input.IsActionJustPressed("drop_block"))
 			{
 				_currentBlock.Release();
-				_spawnTimer.Start();
+				_waitingForLanding = true;
 			}
+		}
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		
+		if (!_waitingForLanding)
+			return;
+		GD.Print($"Velocity: {_currentBlock.LinearVelocity.Length()}  Contacts: {_currentBlock.GetContactCount()}");
+		if (_currentBlock.LinearVelocity.Length() < 1f &&
+			_currentBlock.GetContactCount() > 0)
+		{
+			_waitingForLanding = false;
+			SpawnBlock();
 		}
 	}
 }
