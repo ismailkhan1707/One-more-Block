fc6a2b6e0aa79a506fc992cf81057871326ae67f
ismailkhan1707 <ismailkb2004@gmail.com>
2026-07-22 14:41:48 +0500

    Camera movement added

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System.Collections.Generic;
 
 public partial class GameManager : Node
 {
@@ -7,15 +8,23 @@ public partial class GameManager : Node
 
 	[Export]
 	private Crane _crane;
-
+	
 	[Export]
 	private Node2D _world;
 
+	[Export]
+	private Camera2D _camera;
+
 	private Block _currentBlock;
+	private List<Block> _tower = new();
 	private bool _waitingForLanding = false;
+
+	private float _cameraTargetY;
 
 	public override void _Ready()
 	{
+		_cameraTargetY = _camera.GlobalPosition.Y;
+
 		SpawnBlock();
 	}
 
@@ -40,6 +49,16 @@ public partial class GameManager : Node
 				_waitingForLanding = true;
 			}
 		}
+
+		Vector2 cameraPos = _camera.GlobalPosition;
+
+	cameraPos.Y = Mathf.Lerp(
+		cameraPos.Y,
+		_cameraTargetY,
+		3f * (float)delta
+	);
+
+	_camera.GlobalPosition = cameraPos;
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -47,11 +66,18 @@ public partial class GameManager : Node
 		
 		if (!_waitingForLanding)
 			return;
-		GD.Print($"Velocity: {_currentBlock.LinearVelocity.Length()}  Contacts: {_currentBlock.GetContactCount()}");
+		
+		//GD.Print($"Velocity: {_currentBlock.LinearVelocity.Length()}  Contacts: {_currentBlock.GetContactCount()}");
+		
 		if (_currentBlock.LinearVelocity.Length() < 1f &&
 			_currentBlock.GetContactCount() > 0)
 		{
 			_waitingForLanding = false;
+
+			_tower.Add(_currentBlock);
+			//GD.Print($"Tower Height: {_tower.Count}");
+			_cameraTargetY = Mathf.Min(_cameraTargetY, _currentBlock.GlobalPosition.Y);
+
 			SpawnBlock();
 		}
 	}
